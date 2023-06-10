3d686643960e23d46cb907d7b393010693f7c2c7
Bruno Franco <94724432+bantunesfranco@users.noreply.github.com>
2023-06-10 12:43:37 +0000

    added cpp00 ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,9 +6,129 @@
 /*   By: bfranco <bfranco@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/06/09 17:44:19 by bfranco       #+#    #+#                 */
-/*   Updated: 2023/06/09 17:44:34 by bfranco       ########   odam.nl         */
+/*   Updated: 2023/06/10 12:40:52 by codespace     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <ctime>
+#include <iostream>
 
+// Init static member attributes
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+// Default Contructor
+Account::Account( void ){}
+
+// Constuctor
+Account::Account( int initial_deposit )
+{
+	static int i = 0;
+	
+	this->_accountIndex = i;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	_nbAccounts++;
+	_totalAmount+= initial_deposit;
+	i++;
+
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->checkAmount() << ";created" << std::endl;
+}
+
+// Destructor
+Account::~Account( void )
+{
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->checkAmount() << ";closed" << std::endl;
+}
+
+
+// Static Public Methods
+int	Account::getNbAccounts( void )
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	Account::_displayTimestamp();
+	std::cout << " accounts:" << Account::getNbAccounts() << ";total:" << Account::getTotalAmount() \
+	<< ";deposits:" << Account::getNbDeposits() << ";withdrawals:" << Account::getNbWithdrawals() \
+	<< std::endl;
+}
+
+
+// Static Private Methods
+void	Account::_displayTimestamp( void )
+{
+	time_t	now = time(0);
+	tm 		*ltm = localtime(&now);
+	
+	std::cout << "[" << ltm->tm_year << ltm->tm_mon << ltm->tm_mday \
+	<< "_" << ltm->tm_hour << ltm->tm_min << ltm->tm_sec << "]";
+}
+
+
+// Public methods
+void Account::makeDeposit(int deposit)
+{
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";p_amount:" << this->checkAmount();
+	this->_amount += deposit;
+	this->_nbDeposits++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+	std::cout << ";deposit:" << deposit << ";amount:" << this->checkAmount() \
+	<< ";nb_deposits:" << this->_nbDeposits  << std::endl;
+}
+
+int		Account::checkAmount( void ) const
+{
+	return (this->_amount);
+}
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";p_amount:" << this->checkAmount();
+	if (withdrawal >= this->checkAmount())
+	{
+		std::cout << ";withdrawal:refused" << std::endl;
+		return (false);
+	}
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals++;
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
+	std::cout << ";withdrawal:" << withdrawal << ";amount:" << this->checkAmount() \
+	<< ";nb_withdrawals:" << this->_nbWithdrawals  << std::endl;
+	return (true);
+}
+
+void	Account::displayStatus( void ) const
+{
+	Account::_displayTimestamp();
+	std::cout << " index:" << this->_accountIndex << ";amount:" << this->checkAmount() \
+	<< ";deposits:" << this->_nbDeposits << ";withdrawals:" << this->_nbWithdrawals \
+	<< std::endl;
+}
